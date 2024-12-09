67ebee458e78eaa5049bb2576ff601b78a73764d
Ashritha Shankar <ashritha.shankar@solitontech.com>
2024-12-09 15:49:43 +0530

    feat: src: add child process
    Signed-off-by: Ashritha Shankar <ashritha.shankar@solitontech.com>

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ const webExtensionConfig = {
       // see https://webpack.js.org/configuration/resolve/#resolvefallback
       // for the list of Node.js core module polyfills.
       assert: require.resolve("assert"),
-      path: require.resolve("path-browserify"),
     },
   },
   module: {
