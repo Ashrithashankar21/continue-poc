f96f5a6b61c971ff590b1748b3d94d360fbfa002
Ashritha Shankar <ashritha.shankar@solitontech.com>
2024-12-09 12:41:33 +0530

    feat: src: add fs extension
    Signed-off-by: Ashritha Shankar <ashritha.shankar@solitontech.com>

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-# Hello World Extension
+# FS Extension
 
-This extension prints Hello world.
-It is a simple demonstration of creating a basic extension.
+This extension creates a file in local and append contents to it.
+It is a simple demonstration of creating a basic web extension using fs.
+
+## Efforts and time needed
+
+Around 80 Files need to be changed in 300+ places. - 2 days of effort will be
+required to change each fs functionalities.
+
+## Methods used in fs
+
+1. mkdirsync - done in the POC
+2. writefilesync - done in the POC
+3. readfilesync
+4. cpsync
+5. copyfilesync
+6. appendfilesync
+7. existssync
+8. rmsync
+9. rmdirsync
+10. unlinksync
+11. readdir
+12. readfile
+13. rm
+14. statsync
+15. writestream
+16. stat
+
+## Conclusion
+
+We can migrate the fs by changing each line of code wherever we use fs,
+but not sure if it can produce any bugs in the application after all the
+files are changed. I got lot of ts errors after migrating it to workspace.fs
+and errors like "it will always return true".
+
+![Error](images/error.png)
--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,10 +1,30 @@
 import * as vscode from 'vscode';
+import * as os from 'os';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "helloworld" is now active!');
 
 	const disposable = vscode.commands.registerCommand('helloworld.helloWorld', async () => {
-		vscode.window.showInformationMessage('Hello World!');
+		const homeDir = os.homedir();
+		const targetDir = path.join(homeDir, 'poc-fs');
+		try {
+			await vscode.workspace.fs.createDirectory(vscode.Uri.file(path.join(targetDir, 'data')));
+			await vscode.workspace.fs.writeFile(
+				vscode.Uri.file(path.join(targetDir, 'data.json')),
+				new TextEncoder().encode(JSON.stringify(
+				{
+					name: "binary",
+					version: "1.0.0",
+					author: "Continue Dev, Inc",
+					license: "Apache-2.0",
+				}),
+				),
+		  	);
+			vscode.window.showInformationMessage('File created successfully.');
+		} catch (error) {
+			vscode.window.showErrorMessage(`Error: ${(error as Error).message}`);
+		}
 	});
 
 	context.subscriptions.push(disposable);
