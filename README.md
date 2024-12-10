115d3472592ce6c6653b37e4b9d0d2952468f976
Ashritha Shankar <ashritha.shankar@solitontech.com>
2024-12-10 10:54:42 +0530

    feat: src: add perf hooks
    Signed-off-by: Ashritha Shankar <ashritha.shankar@solitontech.com>

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# FS Extension
+# Perf hooks Extension
 
-It is a simple demonstration of creating a basic web extension using os module.
+It is a simple demonstration of creating a basic web extension using perf hooks module.
 
 ## Conclusion
 
-Migration is simple by adding a resolve fallback.
+Migration for performance measurement is simple but for garbage collection
+we need to find alternatives.
--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,13 +1,14 @@
 import * as vscode from 'vscode';
-import * as os from 'os';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "helloworld" is now active!');
 
 	const disposable = vscode.commands.registerCommand('helloworld.helloWorld', async () => {
-        const platform = os.platform();
-        const architecture = os.arch();
-        vscode.window.showInformationMessage(`Platform: ${platform}, Architecture: ${architecture}`);
+        const start = Date.now();
+        const userAgent = navigator.userAgent;
+        vscode.window.showInformationMessage(`User Agent: ${userAgent}`);
+        const end = Date.now();
+        console.log(`Command execution time: ${end - start} milliseconds`);
 	});
 
 	context.subscriptions.push(disposable);
